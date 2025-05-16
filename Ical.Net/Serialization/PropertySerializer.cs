813ab7fdfd95a863c23b15f30dee4436db0f6387
NRG-Drink <beatdz93@hotmail.com>
2025-05-16 15:20:20 +0200

    chore: fix but net version is broken

--- Ical.Net/Serialization/PropertySerializer.cs
+++ Ical.Net/Serialization/PropertySerializer.cs
@@ -31,45 +31,15 @@ namespace Ical.Net.Serialization
             var sf = GetService<ISerializerFactory>();
 
             var result = new StringBuilder();
-            foreach (var v in prop.Values.Where(value => value != null))
+            if (prop.Name == "CATEGORIES")
             {
-                // Get a serializer to serialize the property's value.
-                // If we can't serialize the property's value, the next step is worthless anyway.
+                var v = prop.Values;
                 var valueSerializer = sf.Build(v.GetType(), SerializationContext) as IStringSerializer;
-
-                // Iterate through each value to be serialized,
-                // and give it a property (with parameters).
-                // FIXME: this isn't always the way this is accomplished.
-                // Multiple values can often be serialized within the
-                // same property.  How should we fix this?
-
-                // NOTE:
-                // We Serialize the property's value first, as during
-                // serialization it may modify our parameters.
-                // FIXME: the "parameter modification" operation should
-                // be separated from serialization. Perhaps something
-                // like PreSerialize(), etc.
                 var value = valueSerializer.SerializeToString(v);
-
-                // Get the list of parameters we'll be serializing
                 var parameterList = prop.Parameters;
                 if (v is ICalendarDataType)
                 {
                     parameterList = (v as ICalendarDataType).Parameters;
-                }
-
-                //This says that the TZID property of an RDATE/EXDATE collection is owned by the PeriodList that contains it. There's nothing in the spec that
-                //prohibits having multiple EXDATE or RDATE collections, each of which specifies a different TZID. What *should* happen during serialization is
-                //that we should work with a single collection of zoned datetime objects, and we should create distinct RDATE and EXDATE collections based on
-                //those values. Right now, if you add CalDateTime objects, each of which specifies a different time zone, the first one "wins". This means
-                //application developers will need to handle those cases outside the library.
-                if (v is PeriodList)
-                {
-                    var typed = (PeriodList)v;
-                    if (!string.IsNullOrWhiteSpace(typed.TzId) && parameterList.All(p => string.Equals("TZID", p.Value, StringComparison.OrdinalIgnoreCase)))
-                    {
-                        parameterList.Set("TZID", typed.TzId);
-                    }
                 }
 
                 var sb = new StringBuilder();
@@ -91,6 +61,71 @@ namespace Ical.Net.Serialization
 
                 result.Append(TextUtil.FoldLines(sb.ToString()));
             }
+            else
+            {
+                foreach (var v in prop.Values.Where(value => value != null))
+                {
+                    // Get a serializer to serialize the property's value.
+                    // If we can't serialize the property's value, the next step is worthless anyway.
+                    var valueSerializer = sf.Build(v.GetType(), SerializationContext) as IStringSerializer;
+
+                    // Iterate through each value to be serialized,
+                    // and give it a property (with parameters).
+                    // FIXME: this isn't always the way this is accomplished.
+                    // Multiple values can often be serialized within the
+                    // same property.  How should we fix this?
+
+                    // NOTE:
+                    // We Serialize the property's value first, as during
+                    // serialization it may modify our parameters.
+                    // FIXME: the "parameter modification" operation should
+                    // be separated from serialization. Perhaps something
+                    // like PreSerialize(), etc.
+                    var value = valueSerializer.SerializeToString(v);
+
+                    // Get the list of parameters we'll be serializing
+                    var parameterList = prop.Parameters;
+                    if (v is ICalendarDataType)
+                    {
+                        parameterList = (v as ICalendarDataType).Parameters;
+                    }
+
+                    //This says that the TZID property of an RDATE/EXDATE collection is owned by the PeriodList that contains it. There's nothing in the spec that
+                    //prohibits having multiple EXDATE or RDATE collections, each of which specifies a different TZID. What *should* happen during serialization is
+                    //that we should work with a single collection of zoned datetime objects, and we should create distinct RDATE and EXDATE collections based on
+                    //those values. Right now, if you add CalDateTime objects, each of which specifies a different time zone, the first one "wins". This means
+                    //application developers will need to handle those cases outside the library.
+                    if (v is PeriodList)
+                    {
+                        var typed = (PeriodList)v;
+                        if (!string.IsNullOrWhiteSpace(typed.TzId) && parameterList.All(p => string.Equals("TZID", p.Value, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            parameterList.Set("TZID", typed.TzId);
+                        }
+                    }
+
+                    var sb = new StringBuilder();
+                    sb.Append(prop.Name);
+                    if (parameterList.Any())
+                    {
+                        // Get a serializer for parameters
+                        var parameterSerializer = sf.Build(typeof(CalendarParameter), SerializationContext) as IStringSerializer;
+                        if (parameterSerializer != null)
+                        {
+                            // Serialize each parameter
+                            // Separate parameters with semicolons
+                            sb.Append(";");
+                            sb.Append(string.Join(";", parameterList.Select(param => parameterSerializer.SerializeToString(param))));
+                        }
+                    }
+                    sb.Append(":");
+                    sb.Append(value);
+
+                    result.Append(TextUtil.FoldLines(sb.ToString()));
+                }
+            }
+
+
 
             // Pop the object off the serialization context.
             SerializationContext.Pop();
